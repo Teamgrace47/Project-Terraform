3325c1555ea0c13ea87f9dc39775d56e9b23112a
Teamgrace47 <114419047+Teamgrace47@users.noreply.github.com>
2024-02-18 20:17:54 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,12 @@ resource "aws_security_group" "jenkins-sg-2022" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
  ingress {
     from_port   = 22
