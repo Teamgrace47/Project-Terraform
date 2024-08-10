d2a5169c7f737712bfd2cefce059e275974e7d11
Teamgrace47 <114419047+Teamgrace47@users.noreply.github.com>
2024-08-10 20:17:32 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-pro-bucket"
+    bucket = "june2024class"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-tf-temp-table"
+    dynamodb_table = "june2024class"
   }
 }
